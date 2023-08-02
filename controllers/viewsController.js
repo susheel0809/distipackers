cc88d02618e0d2aa6e527c176c8641f7de78622c
susheel kumar <susheel@spliceglobal.com>
2023-08-03 01:42:56 +0530

    warehouse three updated

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -54,9 +54,27 @@ exports.warehouseOneTwo = async (req, res) => {
 
 // function getData()
 
-exports.warehouseThree = (req, res) => {
+exports.warehouseThree = async (req, res) => {
+  var { data } = await axios.get(
+    'https://script.googleusercontent.com/macros/echo?user_content_key=qAfwgfrHfVlvuOcPtbDDJbjx_YYvaPttyZNYCHSahdjSQczRr2qFgDFj86n2JHjhS_2-UwyflFRRhuciLTLo-lFIQ3LPZCFPm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGRBB_UEiI4Dln0fnBFqm76EnHOce_ZDP3yX8Dv7nU_2DVd4Ficzn8kbzh-ikD2p_vQNL2VYtVD5-0TKUMjmvFUmCMgUb9Pau9z9Jw9Md8uu&lib=MzFsmOfzUWvArIz4A7cM7CTWkEKQozq2-'
+  );
+  console.log(data);
+  stuffingDataSet = data;
+  console.log('----------');
+  console.log(stuffingDataSet);
+
+  var { data } = await axios.get(
+    'https://script.googleusercontent.com/macros/echo?user_content_key=YBiD6qDyLRHAN9qQi_kZeeU7r9cfXRADeuD3MrrpM8RlTU-VWIE4IwbaMkrHv78Na9dhjkI8w2K0bRV_29YQ96YduIp9AQHfm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIys2KwcMpvaW1tDvxIDU5F8xRVvIfvMwKcwpEbkxgqj-U4Tcik2RkCPsWSu1XxTnpEky7NMoliHTARZMXhJX7asnhEVI65Uc9z9Jw9Md8uu&lib=MzFsmOfzUWvArIz4A7cM7CTWkEKQozq2-'
+  );
+  console.log(data);
+  console.log('********');
+  projectDetails = data;
+  console.log(projectDetails);
+
   res.status(200).render('warehouseThree', {
     title: 'Warehouse Three',
+    stuffingReportWarehouseThree: stuffingDataSet.data,
+    stuffingProjectDetails: projectDetails.data,
   });
 };
 
